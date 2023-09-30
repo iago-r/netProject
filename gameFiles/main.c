a5e4566f92981948ad98adf0e078a6b68474580d
Iago Rios <iago.atlas@outlook.com>
2023-09-30 02:12:13 -0300

    last game scanning implemented, next steps: convert input into a struct

--- gameFiles/main.c
+++ gameFiles/main.c
@@ -2,14 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct action {
+    int type;
+    int coordinates[2];
+    int board[4][4];
+};
+
+
 int main(int argc, char **argv)
 {
 
     FILE *fptr;
     fptr = fopen(*(&argv[1]), "r");
-    if(fptr == NULL) {
+    if (fptr == NULL)
+    {
         printf("File not openned!");
     }
+
+    int answer[4][4];
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            fscanf(fptr, "%i,", &answer[i][j]);
+        }
+    }
+/* 
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%5d\t", answer[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+ */
 
     // CURRENT GAME STATE
     // INIT ALL VALUES WITH -2
@@ -31,47 +59,6 @@ int main(int argc, char **argv)
         printf("\n");
     }
     printf("\n");
-
-/* 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%5d\t", answer[i][j]); // Dont forget to set two tabs in the printings \t\t
-        }
-        printf("\n");
-    }
- */
-/* 
-    for (int i = 0; i < 4; i++)
-    {
-        fscanf(fptr, "%d", )
-    }
-     */
-
-/* 
-    while(fscanf("%s,%s,%s,%d", sem, type, title, &value) == 4) {
-    //do something with the read line values
-    } */
-    //int buffer;
-    int answer[4][4];/* = { {1, 2, -1, 1},
-                         {1, -1, 2, 1},
-                         {1, 2, 1, 1},
-                         {0, 1, -1, 1}, }; */
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            fscanf(fptr,"%i,", &answer[i][j]/* buffer */);
-            //answer[i][j] = buffer;
-        }
-    }
-
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            printf("%5d\t", answer[i][j]);
-        }
-        printf("\n");
-    }
-
 
     return 0;
 }
